b4034366d191beabd80007be71fea3b5b10fada0
abrolag <willgodfrey@musically.me.uk>
2020-09-20 20:04:22 +0100

    x,y stored when saved and restored on restart w,y remembered only for session. children NOT repositioned :(

---
+++ src/UI/YoshiWin.cpp
@@ -0,0 +1,49 @@
+/*
+    YoshiWin.cpp
+
+    This file is part of yoshimi, which is free software: you can redistribute
+    it and/or modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either version 2 of
+    the License, or (at your option) any later version.
+
+    yoshimi is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.   See the GNU General Public License (version 2 or
+    later) for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    yoshimi; if not, write to the Free Software Foundation, Inc., 51 Franklin
+    Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+#include "YoshiWin.h"
+#include "Misc/NumericFuncs.h"
+
+#include <iostream>
+
+#include <FL/fl_draw.H>
+#include <FL/x.H>
+
+using func::limit;
+
+YoshiWin::YoshiWin(int x,int y, int w, int h, const char *label) : Fl_Window(x,y,w,h,label)
+{
+    ;
+}
+
+
+YoshiWin::~YoshiWin()
+{
+    ;
+}
+
+
+void YoshiWin::resize(int x, int y, int w, int h)
+{
+    std::cout << "resizing x" << x << "  y" << y << "  w " << w << "  h " << h << std::endl;
+    this->x(x);
+    this->y(y);
+    this->w(w);
+    this->h(h);
+}
